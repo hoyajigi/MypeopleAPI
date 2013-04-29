09885b20b39bcc274214bcea0b47cd55ea52ec82
juakim <jjuakim100@gmail.com>
2013-04-29 15:38:24 +0900

    consumer키 등록

--- config.php
+++ config.php
@@ -7,9 +7,9 @@ $authorize_url = 'https://apis.daum.net/oauth/authorize';
 $access_token_url = 'https://apis.daum.net/oauth/accessToken';
 
 // Consumer 정보 (Consumer를 등록하면 얻어올 수 있음.)
-$consumer_key = "[Consumer 등록을 한 후 각자 넣으세요.]";
-$consumer_secret = "[Consumer 등록을 한 후 각자 넣으세요.]";
-$callback_url = "[Consumer 등록을 한 후 각자 넣으세요.]";
+$consumer_key = "a5c9e452-1632-498b-bb82-5ba7ad38c0b9";
+$consumer_secret = "6fvDqiW5Dygrw5Hgcpfp6QlAUQ_8YC9I8Gv.FCSSqIfhSOBstTnPRw00";
+$callback_url = "http://callback.dev.daum.net/callback.jsp";
 
 // API prefix (보호된 자원이 있는 URL의 prefix)
 $api_url = 'https://apis.daum.net';
@@ -18,5 +18,5 @@ $api_url = 'https://apis.daum.net';
 $oauth = new OAuth($consumer_key, $consumer_secret, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_URI);
 $oauth->disableSSLChecks();
 
-$access_token_file_name = '[access token을 저장해 둘 파일 경로]'; // ex) C:\accesstoken
+$access_token_file_name = 'C:\Users\JuaKim\Desktop\accessToken'; // ex)C:\Users\사용자명\Deskotop
 ?>
